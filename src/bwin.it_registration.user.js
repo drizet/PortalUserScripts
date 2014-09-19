9b79dd00449a58f2975bc7b8af26c472f7156b46
aingievskyi <drizet90@gmail.com>
2014-09-19 16:10:17 +0300

    Fixed copy to clipboard functionality.

--- src/bwin.it_registration.user.js
+++ src/bwin.it_registration.user.js
@@ -8,55 +8,57 @@
 // @grant       GM_setClipboard
 // ==/UserScript==
 
-var userId = getRandomText("test", 4);
+if($("#registration-form").length){
+	var userId = getRandomText("test", 4);
 
-// Personal data
-$("#Input_NameData_FirstName").val(getRandomText(null, false, 10));
-$("#Input_NameData_LastName").val(getRandomText(null, false, 10));
-// Date Of Birth
-$("#Input_BirthData_DateOfBirth_Day").val("1");
-$("#Input_BirthData_DateOfBirth_Month").val("1");
-$("#Input_BirthData_DateOfBirth_Year").val("1992");
-$("#Input_BirthData_DateOfBirth").attr("value", "1/1/1992");
-// Birth data
-$("#Input_BirthData_BirthCountry").val("IT");
-setTimeout(function(){
-    $("#Input_BirthData_BirthProvince").val("AG").click();
-}, 3000);
-setTimeout(function(){
-    $("#Input_BirthData_BirthCity").val("A089").click();
-}, 4500);
-$("#ConfirmCodiceFiscale #Input_BirthData_FiscalCodeConfirmed").attr("checked", "checked");
-$("#Input_AddressData_AddressState").val("AG");
-$("#Input_AddressData_AddressLine1").val("address");
-$("#Input_AddressData_AddressLine2").val("address2");
-$("#Input_AddressData_AddressZip").val("12312");
-$("#Input_AddressData_AddressCity").val("cityName");
-$("#Input_ContactData_EmailAddress").val(getEmail(8, "bwin.it"));
-$("#Input_ContactData_PhoneNumber").val("1231231");
+	// Personal data
+	$("#Input_NameData_FirstName").val(getRandomText(null, false, 10));
+	$("#Input_NameData_LastName").val(getRandomText(null, false, 10));
+	// Date Of Birth
+	$("#Input_BirthData_DateOfBirth_Day").val("1");
+	$("#Input_BirthData_DateOfBirth_Month").val("1");
+	$("#Input_BirthData_DateOfBirth_Year").val("1992");
+	$("#Input_BirthData_DateOfBirth").attr("value", "1/1/1992");
+	// Birth data
+	$("#Input_BirthData_BirthCountry").val("IT");
+	setTimeout(function(){
+		$("#Input_BirthData_BirthProvince").val("AG").click();
+	}, 3000);
+	setTimeout(function(){
+		$("#Input_BirthData_BirthCity").val("A089").click();
+	}, 4500);
+	$("#ConfirmCodiceFiscale #Input_BirthData_FiscalCodeConfirmed").attr("checked", "checked");
+	$("#Input_AddressData_AddressState").val("AG");
+	$("#Input_AddressData_AddressLine1").val("address");
+	$("#Input_AddressData_AddressLine2").val("address2");
+	$("#Input_AddressData_AddressZip").val("12312");
+	$("#Input_AddressData_AddressCity").val("cityName");
+	$("#Input_ContactData_EmailAddress").val(getEmail(8, "bwin.it"));
+	$("#Input_ContactData_PhoneNumber").val("1231231");
 
-// Account data
-$("#Input_LoginData_Username").val(userId);
-$("#Input_LoginData_Password").val("123123q");
-$("#Input_LoginData_PasswordConfirmation").val("123123q");
-$("#Input_SecurityData_SecurityQuestion").val(11);
-$("#Input_SecurityData_SecurityAnswer").val("123123q");
+	// Account data
+	$("#Input_LoginData_Username").val(userId);
+	$("#Input_LoginData_Password").val("123123q");
+	$("#Input_LoginData_PasswordConfirmation").val("123123q");
+	$("#Input_SecurityData_SecurityQuestion").val(11);
+	$("#Input_SecurityData_SecurityAnswer").val("123123q");
 
-// ID document
-$("#Input_IdentificationData_DocumentType").val(1);
-$("#Input_IdentificationData_DocumentNumber").val("documentNumber");
-// Release Date
-$("#Input_IdentificationData_DocumentReleaseDate_Day").val("1");
-$("#Input_IdentificationData_DocumentReleaseDate_Month").val("1");
-$("#Input_IdentificationData_DocumentReleaseDate_Year").val("1992");
-$("#Input_IdentificationData_DocumentReleaseDate").attr("value", "1/1/1992");
-$("#Input_IdentificationData_DocumentReleasedBy").val("1");
-$("#Input_IdentificationData_DocumentReleaseLocation").val("releaseLocation");
+	// ID document
+	$("#Input_IdentificationData_DocumentType").val(1);
+	$("#Input_IdentificationData_DocumentNumber").val("documentNumber");
+	// Release Date
+	$("#Input_IdentificationData_DocumentReleaseDate_Day").val("1");
+	$("#Input_IdentificationData_DocumentReleaseDate_Month").val("1");
+	$("#Input_IdentificationData_DocumentReleaseDate_Year").val("1992");
+	$("#Input_IdentificationData_DocumentReleaseDate").attr("value", "1/1/1992");
+	$("#Input_IdentificationData_DocumentReleasedBy").val("1");
+	$("#Input_IdentificationData_DocumentReleaseLocation").val("releaseLocation");
 
-$("#Input_TermsAndConditions_TacAcceptance, #Input_PrivacyPolicy_PrivacyPolicyAccepted").attr("checked", "checked");
-$("#Captcha_Input_Answer").val("+++");
+	$("#Input_TermsAndConditions_TacAcceptance, #Input_PrivacyPolicy_PrivacyPolicyAccepted").attr("checked", "checked");
+	$("#Captcha_Input_Answer").val("+++");
 
-GM_setClipboard(userId, "text");
+	GM_setClipboard(userId, "text");
+}
 
 function getEmail(symbolsCount, domain) {
     return getRandomText(null, true, symbolsCount) + "@" + domain;

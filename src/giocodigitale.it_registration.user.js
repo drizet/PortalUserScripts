656c076e8f281a8b506b3f3eec6d4a7282ac7191
aingievskyi <drizet90@gmail.com>
2014-09-25 17:05:07 +0300

    Updated versions.

--- src/giocodigitale.it_registration.user.js
+++ src/giocodigitale.it_registration.user.js
@@ -4,7 +4,7 @@
 // @namespace   giocodigitale.it registration
 // @description prefill registration form
 // @include     *giocodigitale.it/*/registration
-// @version     1
+// @version     3
 // @grant       GM_setClipboard
 // ==/UserScript==
 

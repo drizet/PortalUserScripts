8cd5b24006a2681375ef6090c9e4a1612d534953
aingievskyi <drizet90@gmail.com>
2015-01-21 19:57:45 +0200

    Added disable efforts script for V1

---
+++ src/V1/v1.disable_efforts.user.js
@@ -0,0 +1,17 @@
+﻿// ==UserScript==
+// @name         Disable story and defects efforts and todo's
+// @namespace    Razor
+// @version      0.5
+// @match        http://v1.bwinparty.corp/V1-Production/Default.aspx?*DetailTrackingPage*
+// @grant        none
+// ==/UserScript==
+
+$(function () {
+    $("input[_v1_updater_new_key='_new_SimpleStoryEffortColumn']").prop('disabled', 'disabled');
+    $("input[_v1_updater='Story.ToDo']").prop('disabled', 'disabled');
+    $("span[_v1_updater_new_key='_new_StoryEffortColumn']").find("input, select").prop('disabled', 'disabled');
+
+    $("input[_v1_updater_new_key='_new_SimpleDefectEffortColumn']").prop('disabled', 'disabled');
+    $("input[_v1_updater='Defect.ToDo']").prop('disabled', 'disabled');
+    $("span[_v1_updater_new_key='_new_DefectEffortColumn']").find("input, select").prop('disabled', 'disabled');
+});

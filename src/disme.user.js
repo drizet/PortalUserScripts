91e9935f0b856378eacd1e5c5cf450dd85362296
okushnir <oleksandr.kushnir@win-interactive.com>
2015-06-17 12:44:36 +0300

    updated version; fixed issue if page was refreshed

--- src/disme.user.js
+++ src/disme.user.js
@@ -2,7 +2,7 @@
 // @name        Disme QA2 ALL Labels
 // @namespace   Disme
 // @include     https://disme.bwin.corp/out/out.Deploy.php
-// @version     1
+// @version     1.1
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.js
 // @grant       none
 // ==/UserScript==
@@ -11,8 +11,11 @@ $(document).ready(function() {
   var deployServer = "ATVD1WWQA0205";
   
    // Find all labels except: WPOR, PMU.FR and PP.FR
-   var inputs = $("#form1 td:contains('home.'):not(:contains('WPOR'),:contains('PMU.FR'),:contains('PP.FR'))");
+   var targets = $("#form1 td:contains('home.'):not(:contains('WPOR'),:contains('PMU.FR'),:contains('PP.FR'))");
 
+   // Uncheck selected targets if page was refreshed.
+   var inputs = targets.parent().find("input:checked").prop("checked", false);
+   
    var to = 0;
    var from = 0;
   
@@ -20,7 +23,7 @@ $(document).ready(function() {
 
    while (!receivedUserResponse)
    {
-      var userResponse = prompt(inputs.length+" Portal labels were found.\n\nFirst part of servers = 1\nSecond part of servers = 2", "1")
+      var userResponse = prompt(targets.length+" Portal labels were found.\n\nFirst part of servers = 1\nSecond part of servers = 2", "1")
      
       // 'Cancel' button clicked
       if(userResponse == null)
@@ -29,21 +32,21 @@ $(document).ready(function() {
       switch(parseInt(userResponse))
       {
         case 1:
-            to = Math.floor(inputs.length/2);
+            to = Math.floor(targets.length / 2);
             receivedUserResponse = true;
             break;
         case 2:
-           from = Math.floor(inputs.length/2);
-           to = inputs.length;
+           from = Math.floor(targets.length / 2);
+           to = targets.length;
            receivedUserResponse = true;
            break; 
         default:
            alert("Value is incorrect. Please enter 1 or 2.")
       }
    }
-  
+    
   // Select apropriate labels
-  inputs.slice(from, to).parent().find("input").click(); 
+  targets.slice(from, to).parent().find("input").click(); 
 
   // Set Stage=Development-QA
   $("#stageid option[value='8']").prop("selected", true).click();

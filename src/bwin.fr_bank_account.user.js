e68abf0c9dcd3f16080d753f2ced3f6ab937bb3b
Artur Inzhyievskyi <Artur.Inzhyievskyi@ext.win-interactive.com>
2015-09-09 18:41:46 +0300

    Fixed bank account script

--- src/bwin.fr_bank_account.user.js
+++ src/bwin.fr_bank_account.user.js
@@ -2,15 +2,18 @@
 // @name        bwin.fr bank account
 // @namespace   bwin.fr
 // @description prefill bank account
-// @include     *www.bwin.fr/*/account/bankaccountregistration*
-// @version     2.1.2
+// @include     *www.bwin.fr/*/account/bankaccountregistration/index*
+// @version     2.1.3
 // @grant       none
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.js
 // @require     https://raw.githubusercontent.com/drizet/PortalUserScripts/master/src/core/common.js
 // ==/UserScript==
 
 $(function () {
-    $("#Input_DepositLimit, #Input_BettingStakes, #Input_PokerStakes, #Input_FromAccountBalance, #Input_RemainingBalance").selectOptionByIndex(1);
+    $("#Input_DepositLimit").val(1250);
+    $("#Input_BettingStakes, #Input_PokerStakes").val(100000);
+    $("#Input_FromAccountBalance").val(10000);
+    $("#Input_RemainingBalance").val(5000);
     $("#Input_Iban").val("FR1420041010050500013M02606");
     $("#Input_BicSwift").val("SOGEFRPP");
 });

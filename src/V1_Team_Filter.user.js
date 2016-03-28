a14bd6879885bb2fbaa21f4bdd9090a078a6fb81
Oleksandr Kushnir <oleksandr.kushnir@win-interactive.com>
2016-03-28 22:52:37 +0300

    changed script location

--- src/V1_Team_Filter.user.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name        V1 Team Filter
-// @include     *v1.bwinparty.corp/V1-Production/*Page=Widgets/Popups/MoveToTeam
-// @version     1.0.0
-// @grant       none
-// @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.js
-
-$("select[_v1_updater='PrimaryWorkitem.Team'] option[value!='Team:3153474'][value!='Team:4363681']").remove()

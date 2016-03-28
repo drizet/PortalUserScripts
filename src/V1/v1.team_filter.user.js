54e2ceadf1d6ef124f061f3bfd67e7417be665cd
Oleksandr Kushnir <oleksandr.kushnir@win-interactive.com>
2016-03-28 23:06:49 +0300

    fixed the script

--- src/V1/v1.team_filter.user.js
+++ src/V1/v1.team_filter.user.js
@@ -1,8 +1,10 @@
 // ==UserScript==
 // @name        V1 Team Filter
+// @namespace   M&M's
 // @include     *v1.bwinparty.corp/V1-Production/*Page=Widgets/Popups/MoveToTeam
-// @version     1.0.0
-// @grant       none
+// @version     0.1
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.js
-
-$("select[_v1_updater='PrimaryWorkitem.Team'] option[value!='Team:3153474'][value!='Team:4363681']").remove()
+// @grant       none
+$(function () {
+	$("select[_v1_updater='PrimaryWorkitem.Team'] option[value!='Team:3153474'][value!='Team:4363681']").remove()
+});

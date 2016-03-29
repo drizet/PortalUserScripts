e4f07376855a24bd6de8c505b44634c084535445
Oleksandr Kushnir <oleksandr.kushnir@win-interactive.com>
2016-03-29 11:43:17 +0300

    fixed url

--- src/V1/v1.team_filter.user.js
+++ src/V1/v1.team_filter.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        V1 Team Filter
 // @namespace   M&M's
-// @include     *v1.bwinparty.corp/V1-Production/*Page=Widgets/Popups/MoveToTeam
-// @version     0.1
+// @include     http://v1.bwinparty.corp/V1-Production/Default.aspx?Page=Widgets/Popups/MoveToTeam*
+// @version     0.2
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.js
 // @grant       none
 // ==/UserScript==
 
 $(function () {
 	$("select[_v1_updater='PrimaryWorkitem.Team'] option[value!='Team:3153474'][value!='Team:4363681']").remove()
-});
+})();
